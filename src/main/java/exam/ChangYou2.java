444376bf6f66c289b5787d8ca50e33f4a9eddb20
zhanghuijie0916 <zhanghuijie0916@gmail.com>
2018-09-15 20:37:56 +0800

    笔试题

--- src/main/java/exam/ChangYou2.java
+++ src/main/java/exam/ChangYou2.java
@@ -1,9 +1,39 @@
 package exam;
 
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class ChangYou2 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            int num = in.nextInt();
+            int[] apples = new int[num];
+            int sum = 0;
+            for(int i = 0; i < num; i++){
+                int a = in.nextInt();
+                sum += a;
+                apples[i] = a;
+            }
+            int avg = sum / num;
+            if(sum % num != 0){
+                System.out.println(-1);
+                return;
+            }
+            int res = 0;
+            for(int n : apples){
+                if(n > avg){
+                    int over = n - avg;
+                    if(over % 2 != 0){
+                        System.out.print(-1);
+                        return;
+                    }else {
+                        res += over / 2;
+                    }
+                }
+            }
+            System.out.println(res);
+        }
     }
 }
