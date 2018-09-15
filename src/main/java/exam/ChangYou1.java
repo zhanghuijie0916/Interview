b65af0076f37650bf93d47fd76641cd9a9886bdf
zhanghuijie0916 <zhanghuijie0916@gmail.com>
2018-09-15 20:33:45 +0800

    笔试题

--- src/main/java/exam/ChangYou1.java
+++ src/main/java/exam/ChangYou1.java
@@ -1,9 +1,30 @@
 package exam;
 
 import java.util.Scanner;
-
-public class ChangYou1 {
+/**
+ * @author ForeverLover
+ */
+public class  ChangYou1 {
     public static void main(String[] args) {
+        long s1 = 1;
+        long s2 = 1;
+        int count;
+        long temp;
         Scanner in = new Scanner(System.in);
+        count = in.nextInt();
+        for (int i = 1; i <= count; i++) {
+            if (i == 1) {
+                System.out.println("第" + i + "个月的兔子对数：" + s1);
+                continue;
+            } else if (i == 2) {
+                System.out.println("第" + i + "个月的兔子对数：" + s2);
+                continue;
+            } else {
+                temp = s2;
+                s2 = s1 + s2;
+                s1 = temp;
+                System.out.println("第" + i + "个月的兔子对数：" + s2);
+            }
+        }
     }
 }
