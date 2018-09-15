3b0d2abb70c50799fa0bd680a33a52a2f777fa3f
zhanghuijie0916 <zhanghuijie0916@gmail.com>
2018-09-15 11:32:38 +0800

    笔试题

--- src/main/java/exam/AiQiYi2.java
+++ src/main/java/exam/AiQiYi2.java
@@ -5,3 +5,47 @@ public class AiQiYi2 {
 
     }
 }
+
+    int main(){
+        struct SEGMENT{
+            int x;
+            int y;
+        };
+        int n,si,ei,pos,k,tmp,i,cur;
+        struct SEGMENT seg[101];
+        while(cin>>n){
+            pos = 0;
+            if(n==0)break;
+            while(n--){
+                cin>>si>>ei;
+                seg[pos].x=si;
+                seg[pos].y=ei;
+                pos++;
+            }
+            //BubbleSort
+            for(k=1;k<pos;k++){
+                for(i=0;i<pos-k;i++){
+                    if(seg[i].y>seg[i+1].y){
+                        //swap
+                        tmp=seg[i+1].y;
+                        seg[i+1].y=seg[i].y;
+                        seg[i].y=tmp;
+                        tmp=seg[i+1].x;
+                        seg[i+1].x=seg[i].x;
+                        seg[i].x=tmp;
+                    }
+                }
+            }
+            //main process
+            cur=seg[0].y;
+            int cnt=1;
+            for(k=1;k<pos;k++){
+                if(seg[k].x>=cur){
+                    cur=seg[k].y;
+                    cnt++;
+                }
+            }
+            cout<<cnt<<endl;
+        }
+        return 0;
+    }
