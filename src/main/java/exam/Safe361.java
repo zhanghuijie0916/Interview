5029c79167d05823e825b631b067e9f33f877045
zhanghuijie0916 <zhanghuijie0916@gmail.com>
2018-09-19 19:46:21 +0800

    笔试题

--- src/main/java/exam/Safe361.java
+++ src/main/java/exam/Safe361.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Safe361 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int i = 11;
+        int j = 5;
+        System.out.println(i/j);
     }
 }
--- src/main/java/exam/T3601.java
+++ src/main/java/exam/T3601.java
@@ -3,7 +3,16 @@ package exam;
 import java.util.Scanner;
 
 public class T3601 {
+    public static int fun(int i,int j){
+        if(i<=0 || j<=0){
+            return 1;
+        }
+        return 2*fun(i-3,j/2);
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int x= fun(15,20);
+        System.out.println(x
+        );
     }
 }
